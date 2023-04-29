8377dfa4105645376e8304d500252e558ae2c845
Matheus <torresmiranda.rj@gmail.com>
2023-04-28 23:20:42 -0300

    test: ensure SUT calls LoadLastEventRepository with correct input

---
+++ tests/check-last-event-status.spec.ts
@@ -0,0 +1,32 @@
+export class CheckLastEventStatus {
+	constructor(
+		private readonly loadLastEventRepository: LoadLastEventRepository,
+	) {}
+
+	public async exec(groupId: string): Promise<void> {
+		await this.loadLastEventRepository.loadLastEvent(groupId);
+	}
+}
+
+export interface LoadLastEventRepository {
+	loadLastEvent(groupId: string): Promise<void>;
+}
+
+export class LoadLastEventRepositoryMock implements LoadLastEventRepository {
+	public groupId?: string;
+
+	public async loadLastEvent(groupId: string) {
+		this.groupId = groupId;
+	}
+}
+
+describe('CheckLastEventStatus', (): void => {
+	it('should get last event data', async (): Promise<void> => {
+		const loadLastEventRepository = new LoadLastEventRepositoryMock();
+		const sut = new CheckLastEventStatus(loadLastEventRepository);
+
+		await sut.exec('any_group_id');
+
+		expect(loadLastEventRepository.groupId).toBe('any_group_id');
+	});
+});
